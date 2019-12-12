beed301ff275808141d3dd713c856407911f28d1
lxxjn0 <ljy3510@gmail.com>
2019-12-12 21:28:45 +0900

    feat : UI 로직인 Input / Output 클래스 구현
    1. input 클래스의 전체적인 구현 완료.
    2. output 클래스의 전체적인 구현 완료.

--- src/main/java/com/lxxjn0/ui/Input.java
+++ src/main/java/com/lxxjn0/ui/Input.java
@@ -6,12 +6,39 @@
 
 package com.lxxjn0.ui;
 
+import java.util.Scanner;
+
 /**
- * 프로그램에서 입력과 관련된 모든 기능을 담당하는 클래스.
+ * 프로그램에서 입력과 관련된 모든 기능을 담당하는 클래스(UI 로직).
  *
  * @author JUNYOUNG LEE (lxxjn0)
  * @version 0.1 2019.12.12
  */
 public class Input {
-    
+    /**
+     * 사용자로부터 입력을 받아올 scanner 객체.
+     */
+    private Scanner scanner;
+
+    /**
+     * 숫자를 입력하라는 요청에 대한 사용자의 입력을 받는 메소드.
+     *
+     * @return 사용자로부터 받은 입력을 반환.
+     */
+    public String receiveNumberInput() {
+        scanner = new Scanner(System.in);
+
+        return scanner.nextLine();
+    }
+
+    /**
+     * 게임 재시작 여부를 입력하라는 요청에 대한 사용자의 입력을 받는 메소드.
+     *
+     * @return 사용자로부터 받은 입력(숫자)을 반환.
+     */
+    public int receiveRestartInput() {
+        scanner = new Scanner(System.in);
+
+        return scanner.nextInt();
+    }
 }
