a252ce05fc3ab87fc6c7bf6ecca3d1556982a5b9
lxxjn0 <ljy3510@gmail.com>
2019-12-12 20:44:20 +0900

    refactor : 클래스 분류 연습을 위한 새로운 클래스 추가
    1. 새롭게 추가되는 Input, Output, GameManager 클래스 추가.
    2. README.md에 추가 리펙토링 진행 부분 추가.

---
+++ src/main/java/com/lxxjn0/baseball/GameManager.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)GameManager.java     0.1 2019.12.12
+ *
+ * Copyright (c) 2019 lxxjn0.
+ */
+
+package com.lxxjn0.baseball;
+
+/**
+ * 게임의 시작과 종료, 재시작을 관리하는 전체 게임의 관리 클래스.
+ *
+ * @author JUNYOUNG LEE (lxxjn0)
+ * @version 0.1 2019.12.12
+ */
+public class GameManager {
+
+}
---
+++ src/main/java/com/lxxjn0/ui/Input.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)Input.java       0.1 2019.12.12
+ *
+ * Copyright (c) 2019 lxxjn0.
+ */
+
+package com.lxxjn0.ui;
+
+/**
+ * 프로그램에서 입력과 관련된 모든 기능을 담당하는 클래스.
+ *
+ * @author JUNYOUNG LEE (lxxjn0)
+ * @version 0.1 2019.12.12
+ */
+public class Input {
+    
+}
